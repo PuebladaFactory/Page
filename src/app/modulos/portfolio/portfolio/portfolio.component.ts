17bdf9b21c7fd76cddd8107a86502a2491d9f2d7
Nicolas Gamarra <nicogamarra2010@gmail.com>
2023-09-08 20:06:29 -0300

    mouse enter and mouse leave

--- src/app/modulos/portfolio/portfolio/portfolio.component.ts
+++ src/app/modulos/portfolio/portfolio/portfolio.component.ts
@@ -11,7 +11,9 @@ export class PortfolioComponent implements OnInit {
     
   }
 
-   public mostrar:boolean = false;
+   public mostrar1:boolean = false;
+   public mostrar2:boolean = false;
+   public mostrar3:boolean = false;
 
  /*  onMouseOver() {
     this.mostrar = true;
@@ -23,28 +25,29 @@ export class PortfolioComponent implements OnInit {
 
   public valorVariable: boolean = false;
 
-  @HostListener('mouseenter', ['$event'])
+  @HostListener('mouseenter'|| 'mouseleave', ['$event'])
   onMouseEnter(event: Event) {
     const targetElement = event.target as HTMLElement;
-    console.log("esto es mostrar: ", this.mostrar);
-    if (targetElement.classList.contains('mi-elemento')) {
+   /*  console.log("esto es mostrar1: ", this.mostrar); */
+    if (targetElement.classList.contains('mi-elemento1')) {
       // El cursor pasó por encima del elemento con la clase 'mi-elemento'
       // Realiza las acciones que desees aquí
-      this.mostrar = true;
+      this.mostrar1 = !this.mostrar1;
+      console.log("esto es mostrar1: ", this.mostrar1);
+    } else if (targetElement.classList.contains('mi-elemento2')) {
+      // El cursor pasó por encima del elemento con la clase 'mi-elemento'
+      // Realiza las acciones que desees aquí
+      this.mostrar2 = !this.mostrar2;
+      console.log("esto es mostrar1: ", this.mostrar2);
+    } else if (targetElement.classList.contains('mi-elemento3')) {
+      // El cursor pasó por encima del elemento con la clase 'mi-elemento'
+      // Realiza las acciones que desees aquí
+      this.mostrar3 = !this.mostrar3;
+      console.log("esto es mostrar1: ", this.mostrar3);
     }
   }
 
 
-  @HostListener('mouseleave', ['$event'])
-  onMouseLeave(event: Event) {
-    const targetElement = event.target as HTMLElement;
-    console.log("esto es mostrar: ", this.mostrar);
-    
-    if (targetElement.classList.contains('mi-elemento')) {
-      // El cursor pasó por encima del elemento con la clase 'mi-elemento'
-      // Realiza las acciones que desees aquí
-      this.mostrar = false;
-    }
-  }
+  
 
 }
