07c3d8e0fc4667d0b9625cbbcf8be58a07ef7707
Nicolas Gamarra <nicogamarra2010@gmail.com>
2023-09-08 17:02:39 -0300

    imagenes

--- src/app/modulos/portfolio/portfolio/portfolio.component.ts
+++ src/app/modulos/portfolio/portfolio/portfolio.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+  
+  ngOnInit(): void {
+    
+  }
+
+   public mostrar:boolean = false;
+
+ /*  onMouseOver() {
+    this.mostrar = true;
+  }
+
+  onMouseOut() {
+    this.mostrar = false;
+  }  */
+
+  public valorVariable: boolean = false;
+
+  @HostListener('mouseenter', ['$event'])
+  onMouseEnter(event: Event) {
+    const targetElement = event.target as HTMLElement;
+    console.log("esto es mostrar: ", this.mostrar);
+    if (targetElement.classList.contains('mi-elemento')) {
+      // El cursor pasó por encima del elemento con la clase 'mi-elemento'
+      // Realiza las acciones que desees aquí
+      this.mostrar = true;
+    }
+  }
+
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave(event: Event) {
+    const targetElement = event.target as HTMLElement;
+    console.log("esto es mostrar: ", this.mostrar);
+    
+    if (targetElement.classList.contains('mi-elemento')) {
+      // El cursor pasó por encima del elemento con la clase 'mi-elemento'
+      // Realiza las acciones que desees aquí
+      this.mostrar = false;
+    }
+  }
 
 }
