9051e78aea6eb04265a14ce6142c4b181be4e718
Nicolas Gamarra <nicogamarra2010@gmail.com>
2023-09-12 19:17:41 -0300

    navbar responsivo

--- src/app/modulos/nav/nav/nav.component.ts
+++ src/app/modulos/nav/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AfterViewInit, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, HostListener, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { CdkScrollable } from '@angular/cdk/scrolling';
 
 @Component({
@@ -9,13 +9,27 @@ import { CdkScrollable } from '@angular/cdk/scrolling';
 })
 export class NavComponent {
   isScrolled = false;
+  navbarImage: string = 'https://i.ibb.co/3cxFRRG/imageedit-56-6976167606.png';
+  scrolledNavbarImage: string = 'https://i.ibb.co/kmYdw3H/Pueblada-Factory-2.png';
 
   @HostListener('window:scroll', [])
   onScroll(): void {
     this.isScrolled = window.scrollY > 0;
+    if (window.scrollY > 0) {
+      this.navbarImage = this.scrolledNavbarImage;
+    } else {
+      this.navbarImage = 'https://i.ibb.co/3cxFRRG/imageedit-56-6976167606.png';
+    }
   }
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
+  desactivarOffcanvas() {
+    const offcanvas = this.el.nativeElement.querySelector('#offcanvasWithBothOptions');
+    if (offcanvas) {
+      this.renderer.removeClass(offcanvas, 'show'); // Quita la clase 'show' para desactivar el offcanvas
+    }
+  
+  }
   
 }
